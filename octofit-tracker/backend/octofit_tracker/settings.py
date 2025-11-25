2aea663dd472677e8d37b5da6587af898d030f94
naveen-bhatia-ghcp <naveen.bhatia@accenture.com>
2025-11-25 17:20:39 +0000

    modified:   octofit-tracker/backend/octofit_tracker/settings.py 	modified:   octofit-tracker/backend/octofit_tracker/views.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-#v=4zae_@qpm#m9nt!pi5o-0h9ocxe03po(9veqpn!_+b7$36v
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow Codespace URL and localhost
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
